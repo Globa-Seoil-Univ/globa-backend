4dda62b381ad963e6d2d38585ea97c9f8f62ef7c
dbstjdqls14 <dbstjdqls14@naver.com>
2024-05-22 03:08:58 +0900

    cuda 셋팅중

--- Spring/Globa/src/main/java/org/y2k2/globa/service/RecordService.java
+++ Spring/Globa/src/main/java/org/y2k2/globa/service/RecordService.java
@@ -312,17 +312,17 @@ public class RecordService {
         if (folderShareEntity == null)
             throw new UnAuthorizedException("This user not deserves");
 
-//        RecordEntity recordEntity = new RecordEntity();
-//        recordEntity.setCreatedTime(LocalDateTime.now());
-//        recordEntity.setSize(size);
-//        recordEntity.setPath(path);
-//        recordEntity.setTitle(title);
-//        recordEntity.setUser(userEntity);
-//        recordEntity.setFolder(folderEntity);
-//
-//        recordRepository.save(recordEntity);
+        RecordEntity recordEntity = new RecordEntity();
+        recordEntity.setCreatedTime(LocalDateTime.now());
+        recordEntity.setSize(size);
+        recordEntity.setPath(path);
+        recordEntity.setTitle(title);
+        recordEntity.setUser(userEntity);
+        recordEntity.setFolder(folderEntity);
 
-        kafkaProducer.send("audio-analyze", "analyze", new KafkaRequestDto(21, userEntity.getUserId()));
+        recordRepository.save(recordEntity);
+
+        kafkaProducer.send("audio-analyze", "analyze", new KafkaRequestDto(recordEntity.getRecordId(), userEntity.getUserId()));
 
         return HttpStatus.CREATED;
     }
--- python-server/util/whisper.py
+++ python-server/util/whisper.py
@@ -3,6 +3,7 @@ from faster_whisper import WhisperModel
 
 from util.log import Logger
 
+import ctypes
 
 class STTResults:
     text: str
@@ -23,6 +24,12 @@ class WhisperManager:
         self.model = WhisperModel(model_size, device="cuda", compute_type="float32")
 
     def stt(self, path: str):
+        print("아잇11111111111111111")
+        print(path)
+        # cudnn_path = "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.3/bin/cudnn_ops_infer64_8.dll"
+        # if os.path.exists(cudnn_path):
+        #     ctypes.cdll.LoadLibrary(cudnn_path)
+        # Could not locate
         segments, info = self.model.transcribe(
             path,
             initial_prompt="너는 이제부터 한국어로 대화하는 회의, 강의, 모임 등 사람들과의 대화를 한글로 변환해야 하는 역할이야.",
@@ -36,9 +43,10 @@ class WhisperManager:
             no_repeat_ngram_size=3,
             vad_parameters=dict(min_silence_duration_ms=500)
         )
-
+        print("222222222222222222222222222")
         results = []
         for segment in segments:
+            print("412435346345345")
             # self.logger.info(f"segment: {segment}, start: {segment.start}, end: {segment}\n")
             result: STTResults = STTResults(
                 text=segment.text,
