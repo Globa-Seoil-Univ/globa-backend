ef324f871b90dc64249fb4fb8c0b89e0532c67e2
seungyong <seungyong20@naver.com>
2024-09-20 03:08:09 +0900

    공부시간 일 단위로 변경

--- Spring/Globa/src/main/java/org/y2k2/globa/repository/StudyRepository.java
+++ Spring/Globa/src/main/java/org/y2k2/globa/repository/StudyRepository.java
@@ -1,6 +1,8 @@
 package org.y2k2.globa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.y2k2.globa.entity.StudyEntity;
 import org.y2k2.globa.entity.UserEntity;
 
@@ -9,7 +11,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StudyRepository extends JpaRepository<StudyEntity, Long> {
-    Optional<StudyEntity> findByCreatedTime(LocalDateTime createdTime);
+    @Query(
+            value = "SELECT * FROM study " +
+                    "WHERE DATE(created_time) = DATE(NOW()) AND user_id = :userId AND record_id = :recordId",
+            nativeQuery = true
+    )
+    Optional<StudyEntity> findByCreatedTime(@Param("userId") Long userId, @Param("recordId") Long recordId);
     List<StudyEntity> findAllByUserUserId(Long user);
     List<StudyEntity> findAllByUserUserIdAndRecordRecordId(Long user,Long recordId);
 }
