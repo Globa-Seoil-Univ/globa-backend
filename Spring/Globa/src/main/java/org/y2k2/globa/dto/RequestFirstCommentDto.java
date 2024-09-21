b77a6b044ff7bcb4713c4af8bf4c82d5188b8553
seungyong <seungyong20@naver.com>
2024-09-22 02:34:26 +0900

    댓글 유효성 검사 수정

--- Spring/Globa/src/main/java/org/y2k2/globa/dto/RequestFirstCommentDto.java
+++ Spring/Globa/src/main/java/org/y2k2/globa/dto/RequestFirstCommentDto.java
@@ -1,9 +1,6 @@
 package org.y2k2.globa.dto;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,11 +11,11 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 public class RequestFirstCommentDto {
     @NotNull(message = "You must request startIdx field")
-    @Positive(message = "You must greater than 0")
+    @Min(value = -1, message = "You must greater equal than 0")
     private final Long startIdx;
 
     @NotNull(message = "You must request endIdx field")
-    @Positive(message = "You must greater than 0")
+    @Min(value = -1, message = "You must greater equal than 0")
     private final Long endIdx;
 
     @NotBlank(message = "You must request content field")
