ac94230cdf8110f6c3a7c3108e04b014d57bf65a
YunSungBin <dbstjdqls14@naver.com>
2025-09-07 05:46:52 +0900

    Section 배치 처리
    - Vast Connection 에러 대응

--- python-server/analyze/section.py
+++ python-server/analyze/section.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from sqlalchemy.orm import Session
+from sqlalchemy.testing.util import total_size
 
 from util.open_ai import OpenAIUtil
 from util.whisper import STTResults
@@ -12,5 +13,24 @@ def add_section(record_id: int, text: List[STTResults], session: Session, lan: s
     open_ai = OpenAIUtil()
 
     section_list = open_ai.get_section(record_id=record_id, stt=text, language=lan)
-    session.add_all(section_list)
+    # session.add_all(section_list)
+    total_size = len(section_list)
+
+    batch_size = 4
+
+    for i in range(0, total_size, batch_size):
+        batch = section_list[i:i + batch_size]
+
+        try :
+            session.add_all(batch)
+            session.flush()
+        except Exception as e:
+            session.rollback()
+            for j, section in enumerate(batch):
+                try :
+                    session.add(section)
+                    session.flush()
+                except Exception as e:
+                    session.rollback()
+
     session.flush()
