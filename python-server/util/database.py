6492113d0996bb57bb59c06744a20e45de4189e2
YunSungBin <dbstjdqls14@naver.com>
2025-09-09 23:58:13 +0900

    Database Connection configure 설정
    - RDS Parameter 그룹 변경으로 적용되지 않던 부분 해결

--- python-server/util/database.py
+++ python-server/util/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv('database-url')
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True, pool_timeout=30, connect_args={"connect_timeout": 600, "read_timeout": 600, "write_timeout": 600})
 SessionMaker = sessionmaker(autoflush=False, bind=engine)
 
 
